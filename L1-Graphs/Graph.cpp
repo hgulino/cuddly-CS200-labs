78c3080244c2623d3745bb60f5699c67439e837f
Hunter Gulino <47267453+hgulino@users.noreply.github.com>
2021-02-05 00:15:51 +0000

    Added default files and updated Graph.h

---
+++ L1-Graphs/Graph.cpp
@@ -0,0 +1,40 @@
+/*
+    (name header)
+*/
+
+#include "Graph.h"
+
+using namespace std;
+
+// Definition function operator<<
+
+
+// Definition default constructor
+
+
+// Definition copy constructor
+// Add "noexcept" at the end of the header.
+
+
+// Definition function operator=
+// Add "noexcept" at the end of the header.
+
+
+// Definition destructor
+
+
+// Definition function createGraph
+
+
+// Definition function isEmpty
+
+
+// Definition function printAllCourses
+
+
+// Definition function printPrerequisites
+
+
+// Definition function clearGraph
+
+
---
+++ L1-Graphs/Graph.h
@@ -0,0 +1,47 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <utility>
+#include <algorithm>
+
+const std::string COURSE_PREFIX = "CS A";
+
+class Graph
+{
+	// Declaration friend function operator<<
+	friend std::ostream& operator<<(std::ostream& out, const Graph& graph);
+
+public:
+	// Declaration default constructor
+	Graph() : graph{}, numOfCourses{0} {}
+
+	// Declarations for the Big Three
+	Graph(const Graph& anotherGraph);
+	Graph& operator=(const Graph& rightSide);
+	~Graph();
+	
+	// Declaration function createGraph
+	void createGraph(const std::vector<std::vector<int>>& vectorToCopy);
+
+	// Declaration function isEmpty
+	
+
+	// Declarations of print functions
+	void printAllCourses() const;
+	void printPrerequisites(const int& courseID) const;
+
+	// Declaration function clearGraph
+	void clearGraph();
+
+private:
+
+	std::map<int, std::set<int>> * graph;
+    int numOfCourses;		
+};
+
+#endif
